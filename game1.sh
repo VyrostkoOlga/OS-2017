8ee71eff312fd91a5753101cd6ca1db4e6ea3781
Olga Vyrostko <vyrostkoolga@gmail.com>
2017-03-11 20:56:32 +0500

    fix-up: clear version

--- game1.sh
+++ game1.sh
@@ -1,42 +1,93 @@
-#!/bin/bash
-#
-# Commands: xy
-
-function tryConnect {
-  echo $mark | nc $host $port
-  if [ $? == 0 ]; then
-    connected=1
-    mark="x"
-    other_mark="o"
-  else
-    mark="o"
-    other_mark="x"
-  fi
-}
-
-function toNet {
-  read L
-  processCommand $mark $L
-  echo $L | nc $host $port
-}
-
-function NetListen {
+function netListen {
   nc -l $port
 }
 
-function fromNet {
-  code=$(NetListen)
-  if [ $code == $mark ]; then
-    connected=1
-    return
-  fi
+function toNet {
+  echo $1 | nc $host $port
+}
 
-  processCommand $other_mark $code
+function tryConnect {
+  #try to establish connection with initial mark
+  mark="x"
+  opponent_mark="o"
+  connected=0
+  toNet $mark
+
+  if [ $? == 0 ]; then
+    #there is an opponent, connection is established
+    connected=1
+    mark="o"
+    opponent_mark="x"
+    newGame
+  else
+    echo "Waiting for connection"
+    connected=1
+    mark=$(netListen)
+    newGame
+  fi
+}
+
+function sendCommand {
+  read L
+  processCommand $mark $L
+  toNet $L
+  if [ $? != 0 ]; then
+    #connection is broken, game is over
+    connected=0
+    valid_turn=0
+    tryConnect
+  fi
+}
+
+function receiveCommand {
+  code=$(netListen)
+  if [ $code == "x" ]; then
+    #connection was broken, now start new connection and new game
+    mark="x"
+    opponent_mark="o"
+    newGame
+    valid_turn=0
+  else
+    processCommand $opponent_mark $code
+  fi
+}
+
+#drawing
+function redraw {
+  tput clear
+  tput cup 0 0
+
+  printf "    1 | 2 | 3\n"
+  printf "  -------------\n"
+  for i in `seq 0 2`; do
+    printf "%-2s| %-2s| %-2s| %-2s|\n" $(($i+1)) ${field[$i*3]} ${field[$i*3+1]} ${field[$i*3+2]}
+    printf "  -------------\n"
+  done
+  printf "You are %s\n" $mark
+}
+
+#logic
+function newGame {
+  for i in `seq 0 9`; do
+    field[$i]="_"
+  done
+  redraw
+
+  current_turn="x"
+}
+
+function changeTurn {
+  if [ $current_turn == "x" ]; then
+    current_turn="o"
+  else
+    current_turn="x"
+  fi
 }
 
 function processCommand {
   local mark=$1
   local comm=$2
+  valid_turn=1
 
   #process command
   if [ ${#comm} -ne 2 ]; then
@@ -153,18 +204,7 @@ function checkIfGameOver {
   fi
 }
 
-function redraw {
-  tput clear
-  tput cup 0 0
-
-  printf "    1 | 2 | 3\n"
-  printf "  -------------\n"
-  for i in `seq 0 2`; do
-    printf "%-2s| %-2s| %-2s| %-2s|\n" $(($i+1)) ${field[$i*3]} ${field[$i*3+1]} ${field[$i*3+2]}
-    printf "  -------------\n"
-  done
-}
-
+# get host and port (parametrized) or print help if needed
 if [ $# -ne 2 ]; then
   if [ $# -eq 1 ] && ([ $1 == "--help" ] || [ $1 == "-h" ]); then
     echo "$0"
@@ -180,57 +220,43 @@ fi
 
 host=$1
 port=$2
-connected=0
-mark="o"
-other_mark="x"
-game_over="_"
-valid_turn=1
-turn=1
-allOccupied=0
 
-tryConnect
+connected=0             # connection monitor, if connection is broken, need to reset all
+valid_turn=1            # turn validation monitor, if = 0, do not need next iteration of the game
+game_over="_"           # game over monitor, containts winner's mark
+allOccupied=0           # field is occupied monitor, if = 1, game should be finished because there is no possible turns although there is no winner
+mark="x"                # player's mark, by default = x (first player)
+opponent_mark="o"       # player's opponent's mark, by default = o (second player)
 
 declare field
-for i in `seq 0 9`; do
-  field[$i]="_"
-done
-redraw
+tryConnect
 
-current_turn=0
-while [ true ]; do
-  checkIfGameOver
-  if [ $allOccupied == 1 ]; then
-    echo "No possible turns"
-    break
-  fi
-
-  if [ $game_over != "_" ]; then
-    echo "Game over"
-    if [ $game_over == $mark ]; then
-      echo "You won"
-    else
-      echo "Your opponent won"
-    fi
-    break
-  fi
-
-  if [ $turn == $current_turn ]; then
+current_turn="x"
+while [ $game_over == "_" ] && [ $allOccupied == 0 ]; do
+  if [ $current_turn == $mark ]; then
     echo "Your turn"
-    toNet
+    sendCommand
   else
-    if [ $connected == 0 ]; then
-      echo "Waiting for the connection"
-    else
-      echo "Waiting for the turn"
-    fi
-    fromNet
+    echo "Waiting for opponent's turn"
+    receiveCommand
   fi
-  if [ $valid_turn == 1 ]; then
-    current_turn=$((!$current_turn))
+
+  if [ $connected == 1 ] && [ $valid_turn == 1 ]; then
     redraw
+    changeTurn
   else
-    if [ $turn == $current_turn ]; then
-      echo "Invalid turn"
-    fi
+    echo "Connection is broken or invalid turn"
   fi
+  checkIfGameOver
 done
+
+if [ $game_over == "_" ]; then
+  echo "Game over: to possible turns"
+  exit 0
+fi
+
+if [ $game_over == $mark ]; then
+  echo "You won"
+else
+  echo "Opponent won"
+fi
